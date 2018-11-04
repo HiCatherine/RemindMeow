b6755e66701642810cd026a3f3428ebf2c0f50ac
Christopher Liu <chrisliu@berkeley.edu>
2018-11-04 03:15:53 -0800

    progress?

---
+++ blocking.js
@@ -0,0 +1,21 @@
+var blocked = `
+
+<style>
+  .banner {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  h5 {
+    font-size: 72px;
+    font-weight: 500;
+  }
+</style>
+<div class="banner">
+  <h5>YEET</h5></div>
+
+`;
+
+$("body").empty();
+$("body").append(blocked);
